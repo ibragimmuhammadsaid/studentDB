ca1898bfae9627267935909430036b3b0e3a4e4d
kura <muhammadsaid.ibragim2004@gmail.com>
2024-12-16 16:54:16 +0500

    modified:   src/Controller/MainController.php 	new file:   templates/main/homepage.html.twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,12 +4,22 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong> myproject </strong>: Hello World!');
+        $myName = 'Muhammad Said';
+
+        $studentInfo = ['ID' => 220299,
+        'Year' => 3,
+        'Status' => 'studying'];
+
+        return $this->render('main/homepage.html.twig', [
+            'name' => $myName,
+            'studentInfo' => $studentInfo,
+        ]);
     }
 }
