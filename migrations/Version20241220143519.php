69f5da36354633507a7a019a65c894d87e54be1d
Muhammad Said <108637197+ibragimmuhammadsaid@users.noreply.github.com>
2025-01-30 18:07:11 +0500

    Update Version20241220143519.php

--- migrations/Version20241220143519.php
+++ migrations/Version20241220143519.php
@@ -19,14 +19,15 @@ final class Version20241220143519 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE student (id INT NOT NULL, name VARCHAR(255) NOT NULL, year INT NOT NULL, status VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE student (
+                                id INT NOT NULL, 
+                                name VARCHAR(255) NOT NULL, 
+                                year INT NOT NULL, status VARCHAR(255) NOT NULL, 
+                                PRIMARY KEY(id)) );
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE students (id INT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, year INT NOT NULL, status LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:simple_array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('DROP TABLE student');
     }
 }
